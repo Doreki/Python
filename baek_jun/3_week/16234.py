106ca496e2dc81a5e8d0c6642ea4bb52c9c8d113
Doreki <idh0325@naver.com>
2023-01-08 16:42:54 +0900

    3 week C

--- baek_jun/3_week/16234.py
+++ baek_jun/3_week/16234.py
@@ -1,36 +1,25 @@
 import sys
 sys.setrecursionlimit(100000)
-
-def is_union(y,x):
-
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-
-        if ny < 0 or nx < 0 or ny >=n or nx >=n:
-            continue
-        diff = abs(adj[y][x] - adj[ny][nx])
-        if l<= diff <=r:
-            union[ny][nx] = True
+input = sys.stdin.readline
 
 def dfs(y,x):
     global count
     global sum
-    if y < 0 or x < 0 or y >= n or x >= n or visited[y][x]==True or union[y][x]==False:
-        return False
 
-    if union[y][x]==True:
-        visited[y][x] = True
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if ny < 0 or nx < 0 or ny >= n or nx >= n or visited[ny][nx]==True:
+            continue
 
-        arr.append([y,x])
-        sum+=adj[y][x]
-        count+=1
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
+        diff = abs(adj[y][x] - adj[ny][nx])
+        if l<=diff<= r:
+            visited[ny][nx] = True
+            arr.append([ny,nx])
+            sum+=adj[ny][nx]
             dfs(ny,nx)
-        return True
-    return False
+
+
 
 n,l,r = map(int,input().split())
 
@@ -42,42 +31,32 @@ for _ in range(n):
 dy = [0,-1,0,1]
 dx = [1,0,-1,0]
 
-union = [[False] * n for _ in range(n)]
 visited = [[False] * n for _ in range(n)]
 
 sum = 0
-count = 0
 result = 0
-flag = False
+flag = True
 while True:
-
+    flag=False
+    visited = [[False] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            is_union(i,j)
-
-    for i in range(n):
-        for j in range(n):
-            if union[i][j] == True:
-                flag = True
-
-    if flag == False:
-        break
-
-    for i in range(n):
-        for j in range(n):
-            if dfs(i,j):
-                avg = sum//count
+            if visited[i][j]==False:
+                arr.clear()
+                visited[i][j] = True
+                arr.append([i,j])
+                sum = adj[i][j]
+                dfs(i,j)
+                if len(arr) == 1:continue
+                avg = sum//len(arr)
                 for a in arr:
                     y,x = a
                     adj[y][x] = avg
-                arr = []
-                count = 0
-                sum =0
+                    flag = True
 
-    visited = [[False] * n for _ in range(n)]
+    if flag==False:
+        break
 
-    union = [[False] * n for _ in range(n)]
     result +=1
-    flag=False
 
 print(result)
