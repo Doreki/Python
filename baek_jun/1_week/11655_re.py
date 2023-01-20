c7b71a98876bd872cd046a56de321a729df93d86
Doreki <idh0325@naver.com>
2023-01-20 23:14:39 +0900

    11655 복습

--- baek_jun/1_week/11655_re.py
+++ baek_jun/1_week/11655_re.py
@@ -1,18 +1,25 @@
 str = input()
-result =""
+ans = ""
+
 for s in str:
-    if s.isupper():
-        num = ord(s) - ord('A')
-        if num >= 13:
-            result += chr(ord(s)-13)
-        else:
-            result += chr(ord(s)+13)
-    elif s.islower():
-        num = ord(s) - ord('a')
-        if num >= 13:
-            result += chr(ord(s) - 13)
-        else:
-            result += chr(ord(s) + 13)
+    if not s.isalpha():
+        ans +=s
     else:
-        result+=s
-print(result)
+        if s.isupper():
+            num = ord(s) - ord('A')
+            if num >= 13:
+                num = num - 13 + ord('A')
+                ans +=chr(num)
+            else:
+                num = num + 13 + ord('A')
+                ans +=chr(num)
+        elif s.islower():
+            num = ord(s) - ord('a')
+            if num >= 13:
+                num = num - 13 + ord('a')
+                ans += chr(num)
+            else:
+                num = num + 13 + ord('a')
+                ans += chr(num)
+print(ans)
+
