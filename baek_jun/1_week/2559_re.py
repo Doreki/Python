aa929b4e36d216cf9c14caf5db0520f748d7ffc2
Doreki <idh0325@naver.com>
2023-01-21 22:35:43 +0900

    2559 복습

--- baek_jun/1_week/2559_re.py
+++ baek_jun/1_week/2559_re.py
@@ -1,9 +1,9 @@
 n,k = map(int,input().split())
+
 num = list(map(int,input().split()))
-ans = sum(num[0:k])
-arr = []
-arr.append(ans)
-for i in range(k,n):
-    ans = ans+num[i] - num[i-k]
-    arr.append(ans)
-print(max(arr))
+ans = [0]*(n-k+1)
+
+ans[0] = sum(num[0:k])
+for i in range(k,len(num)):
+    ans[i-k+1] = ans[i-k] + num[i] - num[i-k]
+print(max(ans))
