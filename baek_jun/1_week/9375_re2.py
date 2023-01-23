210d56502f62dce6526ae96929de23af924e44ad
Doreki <idh0325@naver.com>
2023-01-23 23:02:09 +0900

    9375 복습

--- baek_jun/1_week/9375_re2.py
+++ baek_jun/1_week/9375_re2.py
@@ -1,15 +1,19 @@
 import collections
+import sys
 
+input = sys.stdin.readline
 t = int(input())
 
 for _ in range(t):
     n = int(input())
     arr = []
-    result = 1
+    ans = 1
     for _ in range(n):
         _,cate = input().split()
         arr.append(cate)
     counter = collections.Counter(arr)
+
     for c in counter.values():
-        result *= c+1
-    print(result-1)
+        ans *= c+1
+
+    print(ans-1)
