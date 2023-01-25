6115b13689b8511a3d93921820a6287edf080371
Doreki <idh0325@naver.com>
2023-01-25 15:11:02 +0900

    1940 복습

--- baek_jun/1_week/1940_re.py
+++ baek_jun/1_week/1940_re.py
@@ -1,19 +1,18 @@
 n = int(input())
 target = int(input())
 arr = list(map(int,input().split()))
-arr.sort()
 left = 0
 right = n-1
-sum = 0
 count = 0
-while left!=right:
+arr.sort()
+
+while left<right:
     sum = arr[left]+arr[right]
     if sum==target:
-        count+=1
-        left+=1
-    elif sum>target:
-        right -=1
-    else:
+        count +=1
         left +=1
-
+    elif sum < target:
+        left +=1
+    else:
+        right -=1
 print(count)
